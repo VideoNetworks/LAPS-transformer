924668f8809e5c086ad025f59888ac54f08b8fb4
Hao Zhang <haozhang@Haos-MacBook-Pro.local>
2022-09-16 12:24:15 +0800

    tmp

--- scripts/tmp.sh
+++ scripts/tmp.sh
@@ -4,5 +4,5 @@ import os
 cmd = "python -u main_tokshift.py \
 		--multiprocessing-distributed --world-size 1 --rank 0 \
 		--dist-ur tcp://127.0.0.1:23677 \
-		--cfg_file config/custom/k400/nat/nat2d_8x8.yaml"
+		--cfg_file config/custom/k400/nat/nat2D_8x8.yaml"
 os.system(cmd)
