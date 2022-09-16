39aece9ecbc0416782a91ae555040ce2456ff246
Hao Zhang <haozhang@Haos-MacBook-Pro.local>
2022-09-16 12:18:14 +0800

    add atte

--- scripts/tmp.sh
+++ scripts/tmp.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import os
 ### nat2D_8x8
-#cmd = "python -u main_tokshift.py \
-#		--multiprocessing-distributed --world-size 1 --rank 0 \
-#		--dist-ur tcp://127.0.0.1:23677 \
-#		--cfg_file config/custom/k400/nat/nat2d_8x8.yaml"
-#os.system(cmd)
+cmd = "python -u main_tokshift.py \
+		--multiprocessing-distributed --world-size 1 --rank 0 \
+		--dist-ur tcp://127.0.0.1:23677 \
+		--cfg_file config/custom/k400/nat/nat2d_8x8.yaml"
+os.system(cmd)
